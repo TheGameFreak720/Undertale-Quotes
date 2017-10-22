06c84b81308bb62cd1868539387f3555ff464f05
TheGameFreak720 <dj-entityofthedark@live.com>
2017-10-21 22:01:57 -0400

    Added the pictures into a folder and now it loads faster

--- js/index.js
+++ js/index.js
@@ -11,121 +11,121 @@ switch(getQuote) {
   case 1:
     quote = "\"It's a beautiful day outside. Birds are singing, flowers are blooming. On days like this...kids like you...SHOULD BE BURNING IN HELL.\"" ;
     author = "- Sans";
-    character = "https://media.tenor.co/images/de3cc7fc4ff265ed1f2883581f5a8c98/raw";
+    character = "img/san.gif";
     break;
   
   case 2:
     quote = "\"Now your sanity and mine can die TOGETHER!\"";
     author = "- Undyne";
-    character = "http://vignette3.wikia.nocookie.net/undertale/images/9/99/Undyne_(genocide_armor).gif/revision/20151219024306";
+    character = "img/undyne.gif";
     break;
     
   case 3:
     quote = "\"I'M UNDYNE AND I'M PILING ON THE SMOOCHES!\"";
     author = "- Alphys";
-    character = "http://vignette1.wikia.nocookie.net/vsbattles/images/c/cc/Alphys_embarrassed.gif/revision/latest?cb=20160210092442";
+    character = "img/alphys.gif";
     break;
     
   case 4:
     quote = "\"I should have killed you when I had the chance.\"";
     author = "- Alphys";
-    character = "http://vignette1.wikia.nocookie.net/vsbattles/images/c/cc/Alphys_embarrassed.gif/revision/latest?cb=20160210092442";
+    character = "img/alphys.gif";
     break;
     
   case 5:
     quote = "\"YOU! WILL! NEVER! SPARE ME!\"";
     author = "- Undyne";
-    character = "http://vignette2.wikia.nocookie.net/undertale/images/e/ed/Undyne.gif/revision/latest?cb=20151206073203";
+    character = "img/undyne2.gif";
     break;
     
   case 6:
     quote = "\"I can't go to hell. I'm already out of vacation days.\"";
-    author = "- Burgerpants"
-    character = "https://pbs.twimg.com/media/CRGLYooUAAALiGt.png";
+    author = "- Burgerpants";
+    character = "img/burgerpants.png";
     break;
     
   case 7:
     quote = "\"Our family is bigger than evah. Now that my wife. Is fused with sixteen othah people.\"";
     author = "- Snowdrake's Father";
-    character = "http://vignette4.wikia.nocookie.net/undertale/images/f/f5/Snowdrake.gif/revision/latest?cb=20151218012359";
+    character = "img/Snowdrake.gif";
     break;
     
   case 8:
     quote = "\"In this world, it's kill or be killed.\"";
     author = "- Flowey";
-    character = "https://img.fireden.net/vg/image/1444/32/1444329515579.gif";
+    character = "img/flowey.gif";
     break;
     
   case 9:
     quote = "\"Prove to me that you are strong enough to survive.\"";
     author = "- Toriel";
-    character = "http://vignette2.wikia.nocookie.net/undertale/images/f/f8/TorielBattleHurt.png/revision/latest?cb=20160531102626";
+    character = "img/Toriel.png";
     break;
     
   case 10:
     quote = "\"Get Dunked On!\"";
     author = "- Sans";
-    character = "https://i.imgur.com/Id5IaEp.gif";
+    character = "img/sans2.gif";
     break;
     
   case 11:
     quote = "\"You're a wimpy loser with a big heart!\"";
     author = "- Undyne";
-    character = "http://vignette2.wikia.nocookie.net/undertale/images/9/98/Undynewithoutarmor.gif/revision/latest?cb=20160216032300";
+    character = "img/undyne3.gif";
     break;
     
   case 12:
     quote = "\"I like to just lie on the floor and feel like garbage\"";
     author = "- Napstablook";
-    character = "https://67.media.tumblr.com/026d8c27cd8e4c589bfaba180b233d41/tumblr_nw5i6uiY7d1r9xrggo3_400.gif";
+    character = "img/napstablook.gif";
     break;
     
   case 13:
     quote = "\"There's a burning feeling inside me. A feeling that WON'T let me die.\"";
     author = "- Undyne";
-    character = "http://vignette3.wikia.nocookie.net/undertale/images/9/99/Undyne_(genocide_armor).gif/revision/20151219024306";
+    character = "img/undyne.gif";
     break;
     
   case 14:
     quote = "\"Who needs arms with LEGS like these?!\"";
     author = "- Mettaton";
-    character = "https://cdn2.scratch.mit.edu/get_image/user/6773088_90x90.png?v=1466461674.44";
+    character = "img/metatton.gif";
     break;
     
   case 15:
     quote = "\"It's me, SASS! YOU WANNA HAVE A BAD TOM?!\"";
     author = "- Sans";
-    character = "https://i.imgur.com/Id5IaEp.gif";
+    character = "img/sans2.gif";
     break;
     
   case 16:
     quote = "\"Mew Mew Kissy Cutie 2 is neither kissy nor cutie. It's trash!\"";
     author = "- Alphys";
-    character = "http://vignette1.wikia.nocookie.net/vsbattles/images/c/cc/Alphys_embarrassed.gif/revision/latest?cb=20160210092442";
+    character = "img/alphys.gif";
     break;
     
   case 17:
     quote = "\"You're gonna have a bad time.\"";
     author = "- Sans";
-    character = "https://media.tenor.co/images/de3cc7fc4ff265ed1f2883581f5a8c98/raw";
+    character = "img/san.gif";
     break;
     
   case 18:
     quote = "\"DARK DARKER YET DARKER THE DARKNESS KEEPS GROWING THE SHADOWS CUTTING DEEPER\"";
     author = "- Gaster";
-    character = "https://undertalemysteries.files.wordpress.com/2016/01/hqdefault1.jpg?w=361";
+    character = "img/gaster.jpg";
     break;
     
   case 19:
     quote = "\"Who does Alphys have a crush on?\"";
     author = "- Mettaton";
-    character = "http://vignette1.wikia.nocookie.net/undertale/images/5/58/Mettaton.gif/revision/latest?cb=20151111052225";
+    character = "img/mettaton2.gif";
     break;
     
   default:
     quote = "\"OHHHHHHHH, YESSSSSSSS!\"";
     author = "- Mettaton";
-    character = "https://cdn2.scratch.mit.edu/get_image/user/6773088_90x90.png?v=1466461674.44";
+    character = "img/metatton.gif";
     break;
 }
 
